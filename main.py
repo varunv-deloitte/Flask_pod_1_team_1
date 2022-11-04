952f3ef17837174567bd18ea1ae7d31deeb4e157
varunv-deloitte <varun8@deloitte.com>
2022-11-04 16:40:56 +0530

    Added code for FEMA data extraction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 import requests
 import urllib.parse
+from bs4 import BeautifulSoup
+import json
 
 import os
 import urllib.request
@@ -62,6 +64,7 @@ def getImages(lat, lon):
 
     return "Image retrieved successfully "
 
+
 @app.route("/getnearByplace", methods=['POST'])
 def getNearByPlace():
     if request.method == 'POST':
@@ -70,16 +73,52 @@ def getNearByPlace():
             lan = request.json['lan']
             radius = request.json['radius']
             categories = request.json['categories']
-            apiKey='71e7c3f7389e451fad669ee1ca43b8da'
-            print(lat,lan,radius,categories)
-            nearByPlaceUrl = "https://api.geoapify.com/v2/places?categories=%s&filter=circle:%s,%s,%d&bias=proximity:%s,%s&limit=20&apiKey=%s" %(categories,lan,lat,radius,lan,lat,apiKey)
+            apiKey = '71e7c3f7389e451fad669ee1ca43b8da'
+            nearByPlaceUrl = "https://api.geoapify.com/v2/places?categories=%s&filter=circle:%s,%s,%d&bias=proximity:%s,%s&limit=20&apiKey=%s" % (
+                categories, lan, lat, radius, lan, lat, apiKey)
             print(nearByPlaceUrl)
             nearByplaces = requests.get(nearByPlaceUrl)
             data = nearByplaces.json()
-            print(data)
             return data
         except:
             return "Falild to featch places"
+
+
+@app.route("/getriskindex", methods=['POST'])
+def getRiskIndex():
+    if request.method == 'POST':
+        try:
+            # code = request.json['code']
+            html_text = requests.get("https://hazards.fema.gov/nri/report/viewer?dataLOD=Counties&dataIDs=C23031").text
+            soup = BeautifulSoup(html_text, 'lxml')
+            risk_score = soup.find('span', class_='summary-row-score-value').text
+            risk_list = soup.find('div', class_='risk-list print-together')
+            risk_list_score_list = risk_list.findAll('td', class_="number")
+            risk_list_score_name = risk_list.findAll('td', class_='text')
+            # risk_list_score = risk_list_score_list.find('span', class_='score')
+            score = []
+            heading = []
+
+            f = 1
+            for heading_list in risk_list_score_name:
+                if f:
+                    heading.append(heading_list.text)
+                f = (f + 1) % 2
+
+            for score_list in risk_list_score_list:
+                temp = score_list.find('span').text
+                if (temp == '--'):
+                    temp = '0'
+                score.append(temp)
+            [text,value] = risk_score.split(" ")
+            data = {"Risk_score":value}
+            for i in range(min(len(score),len(heading)) ):
+                data[heading[i]] = score[i]
+
+            return make_response(jsonify(data),200)
+
+        except:
+            return "Unable to find risk index"
 
 
 if __name__ == '__main__':
